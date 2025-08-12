b09b6840808f9d258edae6beaedd88ef25e30343
Hiltjo Posthuma <hiltjo@codemadness.org>
2025-08-12 19:17:20 +0200

    config: make refreshrate for mouse move/resize a config option
    Bump the default from 60 to 120.

--- dwm.c
+++ dwm.c
@@ -1181,7 +1181,7 @@ movemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			if ((ev.xmotion.time - lasttime) <= (1000 / refreshrate))
 				continue;
 			lasttime = ev.xmotion.time;
 
@@ -1335,7 +1335,7 @@ resizemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			if ((ev.xmotion.time - lasttime) <= (1000 / refreshrate))
 				continue;
 			lasttime = ev.xmotion.time;
 
